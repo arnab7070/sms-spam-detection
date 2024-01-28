c66edd2f092552d252db3621a4d21c5bc9a6e26d
Arnab Nandi <arnabnandi7074@gmail.com>
2024-01-28 12:04:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pickle
+
+loaded_model = pickle.load(open('model.pkl', 'rb'))
+loaded_vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+def predict(new_comment): 
+
+    prediction = loaded_model.predict(new_comment)
+    if title: 
+        if prediction[0] == 'spam':
+            st.subheader("This comment is :red[spam].")
+        else:
+            st.subheader("This comment is :green[not spam].")
+    else:
+        st.write("Please enter the comment to check if it is a spam message or not...")
+
+st.title("Comment Spam Detection")
+st.divider()
+title = st.text_input('Enter your message')
+new_comment = loaded_vectorizer.transform([title])
+if st.button("Predict"): predict(new_comment=new_comment)
