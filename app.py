9c91e09940e5327f9599f025e7fd72e1ef340469
Arnab Nandi <arnabnandi7074@gmail.com>
2024-01-28 12:47:47 +0530

    changed source code

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ st.set_page_config(
 loaded_model = pickle.load(open('model.pkl', 'rb'))
 loaded_vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
 
-def predict(new_comment): 
-
+def predict(title): 
+    new_comment = loaded_vectorizer.transform([title])
     prediction = loaded_model.predict(new_comment)
     if title: 
         if prediction[0] == 'spam':
@@ -23,5 +23,5 @@ def predict(new_comment):
 st.title("Comment Spam Detection")
 st.divider()
 title = st.text_input('Enter your message')
-new_comment = loaded_vectorizer.transform([title])
-if st.button("Predict"): predict(new_comment=new_comment)
+if title: predict(title=title)
+if st.button("Predict"): predict(title=title)
